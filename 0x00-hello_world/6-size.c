cda3c0c3653c3b14bc58074107a8090f52c499c4
Said Nali <saidnali.sn@gmail.com>
2023-03-20 17:31:54 -0700

    c hello word advenced

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
-*main- Entry point
-*Return: always 0 ( success)
-*/
+ *main - Entry point
+ *eturn: Always 0 (Success)
+ */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+	int myInt;
+	long int myLInt;
+	long long int myLLInt;
+	char myChar;
+	float myFloat;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(a));
-printf("Size of an int: %lu byte(s)\n", sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-printf("Size of a float: %lu byte(s)\n", sizeof(f));
-return (0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(myLInt));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(myLLInt));
+	printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
+	return (0);
 }
